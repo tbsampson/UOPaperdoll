d35cba2847059234e676520f624121bf4d702d17
tbsampson <tbsampson@gmail.com>
2016-12-17 20:05:50 -0600

    Update config.php

--- Local PHP/config.php
+++ Local PHP/config.php
@@ -7,27 +7,23 @@
  *
  *	License: MIT
  *	Created: 05/2011
- *	Updated: 01/2015
+ *	Updated: 12/17/2016
  */
-
 /*Database*/
-define("DB_SERVER", "127.0.0.1");
+define("DB_SERVER", "localhost");
 define("DB_PORT", "3306");
 define("DB_NAME", "myrunuo");
-define("DB_USER","ultima");
-define("DB_PASS","******");
+define("DB_USER","root");
+define("DB_PASS","");
 
 /*Character Tables*/
-define("TBL_CHARS", "chars");
-define("TBL_CHARS_LAYERS", "chars_layers");
-
+define("TBL_CHARS", "myrunuo_characters");
+define("TBL_CHARS_LAYERS", "myrunuo_characters_layers");
 /*Misc*/
-
 /*
  *	When enabled, outputs plain text debug logs of the entire generation process.
  */
-define("DEBUG", false);
-
+define("DEBUG", true);
 /*
  *	Support client files from High Seas and above. 
  *	Typically client 7.0.9+
@@ -35,7 +31,6 @@ define("DEBUG", false);
  *	TileData.mul uses 8 bytes (Int64) to store the Flags data in clients after and including 7.0.9
  */
 define("POST_HS", true);
-
 /*
  *	Support client files from Age of Shadows and above.
  *	TileData.mul uses 0x400000 for the Wearable flag in clients before AOS.
@@ -43,6 +38,6 @@ define("POST_HS", true);
  *	If POST_HS is true, POST_AOS is ignored.
  */
 define("POST_AOS", true);
-
-
+/*MUL File Path*/
+define("MUL_PATH", "OLD_FILES/");
 ?>
